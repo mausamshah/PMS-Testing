c00a73dd232ef4bbc550b1c436a5755c65fc4930
Ravi Shah <ravi.shah31290@gmail.com>
2026-04-13 15:05:48 +0530

    remove not needed file

--- tests/login/nowauctions.spec.ts
+++ tests/login/nowauctions.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-    // Test 1: Page Load + Screenshot
-    test('Login page loads', async ({ page }) => {
-    await page.goto('https://nowauctions.net/login');
-    
-    await expect(page).toHaveURL(/login/);
-    console.log('URL:', page.url());
+// Test 1: Page Load + Screenshot
+test('Login page loads', async ({ page }) => {
+  await page.goto('https://nowauctions.net/login');
 
-    // Inject URL label into page
-    await page.evaluate(() => {
+  await expect(page).toHaveURL(/login/);
+  console.log('URL:', page.url());
+
+  // Inject URL label into page
+  await page.evaluate(() => {
     const div = document.createElement('div');
     div.innerText = 'Target Test URL: ' + window.location.href;
     div.style.position = 'fixed';
@@ -24,13 +24,13 @@ import { test, expect } from '@playwright/test';
 });
 
 
-    // Test 2: Login Functionality
-    test('Login test - LogicRays Tracker', async ({ page }) => {
-    await page.goto('https://nowauctions.net/login');
-    await page.getByPlaceholder('Email').fill('mayur@logicrays.com');
-    await page.getByPlaceholder('Password').fill('SUPERadmin@1998');
-    await page.getByRole('button', { name: 'Login' }).click();
-    await expect(page).not.toHaveURL(/login/);
-    await page.pause(); // 👈 browser will stop here
-    // Run - npx playwright test nowauctions.spec.ts
+// Test 2: Login Functionality
+test('Login test - LogicRays Tracker', async ({ page }) => {
+  await page.goto('https://nowauctions.net/login');
+  await page.getByPlaceholder('Email').fill('mayur@logicrays.com');
+  await page.getByPlaceholder('Password').fill('SUPERadmin@1998');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page).not.toHaveURL(/login/);
+  await page.pause(); // 👈 browser will stop here
+  // Run - npx playwright test nowauctions.spec.ts
 });
--- vithutest.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test } from '@playwright/test';
-
-test('Capture login page screenshot', async ({ page }) => {
-  await page.goto('https://nowauctions.net/login');
-  await page.screenshot({ path: 'Header.png' });
-});
