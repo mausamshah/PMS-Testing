4e1102f7a8d723970c3d0676a3af4c51dfcc5327
Ravi Shah <ravi.shah31290@gmail.com>
2026-04-17 15:27:32 +0530

    Reg Test Suite

---
+++ utils/apiConfig.ts
@@ -0,0 +1,3 @@
+export const API_TOKEN = 'a2ab6105ef70ca4628eada4701f14d04607c32f59c1831dc53ea4b1f672d699d';
+export const BASE_URL = 'https://gorest.co.in/public/v2';
+export const GET_USERS_ENDPOINT = `${BASE_URL}/users`;
